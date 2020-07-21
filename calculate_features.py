cbd617ef703db97483a2731d1c0e918734e34c48
miaoli_psy <chuoli223@yahoo.com>
2020-07-21 20:05:34 +0200

    add cal feature

---
+++ calculate_features.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 21 20:02:32 2020
+
+@author: Miao
+"""
+#%% =============================================================================
+# intrested features
+# =============================================================================
+
+# calculate features
+def cal_feature_choice(raw_choice):
+    '''
+    calcualte the number of choice 1 from 
+    past 10 trials
+    
+    arg*
+    raw_choice: array of int, eg (1009,)
+    
+    return
+    feature_choice: array of int, eg (1009-10,)
+    '''
+    pass
+    # return
+
+def cal_feature_stim_posi(raw_contrastLeft):
+    '''
+    calculate the number of stimulus that 
+    appeared on the left visual field
+    
+    arg*:
+    return*
+    feature_posi_num:
+    
+    '''
+    pass
+
+
+def cal_feature_stimu_contrast(raw_contrastLeft):
+    pass
+
+def cal_feature_fb(raw_fbtype):
+    pass
+#%% =============================================================================
+# true values --> y
+# =============================================================================
+
+# calculate true values: eg: the respone of current trial is correct or not (0/1)
